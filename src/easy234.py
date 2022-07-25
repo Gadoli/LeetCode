a195ce46ebf0ae086f887c6224240c6fa0564a0c
David <gadoli.dodo@hotmail.fr>
2022-07-25 08:55:03 +0200

    Palindrome Linked List - added from discuss

--- src/easy234.py
+++ src/easy234.py
@@ -16,3 +16,26 @@ def isPalindrome(head):
     n = len(L)
     if n%2==1: return L[:n//2 + 1] == L[n//2:][::-1]
     return L[:n//2] == L[n//2:][::-1]
+
+# from discussion
+def isPalindrome2(head):
+    # rev will contain the first half reversed
+    rev = None
+    
+    # fast permits to loop only half's length of the linkedlist
+    # slow will be positionned at half pos
+    slow = fast = head
+    while fast and fast.next:
+        fast = fast.next.next
+        rev, rev.next, slow = slow, rev, slow.next
+    
+    # fast is at the end, length is odd
+    if fast: slow = slow.next
+    
+    # checking if <- (rev) and -> (slow) are equals
+    while rev and rev.val == slow.val:
+        slow = slow.next
+        rev = rev.next
+    
+    # rev will be empty at the end's loop
+    return not rev
