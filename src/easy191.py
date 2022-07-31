eaefccd12aa4e9d3b7a22611860227898284ac11
David <gadoli.dodo@hotmail.fr>
2022-07-31 23:52:02 +0200

    Number of 1 bits

---
+++ src/easy191.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 31 23:51:01 2022
+
+@author: David
+
+LeetCode - Easy. 191 Number of 1 bits
+"""
+from collections import Counter
+
+def hammingWeight(n)
+    return Counter(bin(n))['1']
