6a17bd24fe0f272dd31a31aaf0288f62d5a9b3a9
David <gadoli.dodo@hotmail.fr>
2022-08-09 22:53:36 +0200

    Linked List Cycle

--- src/easy141.py
+++ src/easy141.py
@@ -10,6 +10,7 @@ Runtime: 62 ms, faster than 86.53% of Python3 online submissions for Linked List
 Memory Usage: 18 MB, less than 10.56% of Python3 online submissions for Linked List Cycle.
 """
 
+# my solution
 def hasCycle(head):
     # creating a set to store seen nodes
         S = set()
@@ -26,4 +27,28 @@ def hasCycle(head):
             else: return True
             
         # no nodes go back to a seen node
+        return False
+    
+
+
+
+# from discussion - commented and understood
+# Tortoise and hare
+def hasCycle2(head):
+    # doing a try because if it's not a cycle then slow or fast may be None
+    try:
+        slow = head
+        fast = head.next
+        
+        # if there's a cycle, eventually slow == fast 
+        # multiple loop in cycle may occur
+        while slow is not fast:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # great success
+        return True
+    
+    # went to a None node
+    except:
         return False
