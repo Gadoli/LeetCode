64f9928d693239133783d99bd0d40332bd4460d3
David <gadoli.dodo@hotmail.fr>
2022-08-24 23:59:26 +0200

    Add Two Integers

---
+++ src/easy2235.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 24 23:58:33 2022
+
+@author: david
+
+LeetCode - 2235. Add Two Integers
+
+did to keep streak
+"""
+
+def sum2(num1, num2):
+        return num1 + num2 
