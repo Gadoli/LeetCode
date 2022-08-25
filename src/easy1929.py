3cd41c8c933cb0f4a4101138ba4d5449d0ac6ca1
David <gadoli.dodo@hotmail.fr>
2022-08-25 15:15:03 +0200

    Concatenation of Array

---
+++ src/easy1929.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 25 15:13:54 2022
+
+@author: david
+
+LeetCode - 1929. Concatenation of Array
+"""
+
+def getConcatenation(nums):
+        return nums + nums
+
