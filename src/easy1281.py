a1b6251f7fc7161f1aa48c41066c456ec15cb9a1
David <gadoli.dodo@hotmail.fr>
2022-08-28 19:30:04 +0200

    Subtract the Product and Sum ofDigits of anInteger

--- src/easy1281.py
+++ src/easy1281.py
@@ -7,6 +7,8 @@ Created on Sun Aug 28 19:20:52 2022
 LeetCode - Easy - 1281. Subtract the Product and Sum of Digits of an Integer
 """
 
+import math
+
 def subtractProductAndSum(n):
     L = [int(i) for i in list(str(n))]
     return math.prod(L) - sum(L)
