8c7657978ac78e1471143bcdd2edcefb0e0cccf3
David <gadoli.dodo@hotmail.fr>
2023-02-12 11:46:06 +0100

    Convert the Temperature

---
+++ src/easy2469.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 11:44:31 2023
+
+@author: david
+
+LeetCode - Easy 2469. Convert the Temperature
+
+"""
+
+def convertTemperature(celsius: float):
+    return celsius + 273.15, celsius * 1.80 + 32.00
