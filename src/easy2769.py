ff9adf9a0f19b9625b6510458f56424c4113f672
David <gadoli.dodo@hotmail.fr>
2023-07-23 23:03:41 +0200

    Easy 2769. Find the Maximum Achievable Number

---
+++ src/easy2769.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 23 23:03:01 2023
+
+@author: david
+
+LeetCode - Easy 2769. Find the Maximum Achievable Number
+
+"""
+
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + 2*t
